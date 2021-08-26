d8be5b9adffe4d421eb905135be4041db3ea26d5
Jaime Castro <jandrescastro97@gmail.com>
2021-08-25 21:45:10 -0500

    add frontend class 25/08/2021

---
+++ frontend/src/app/home/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+
+  public registerData: any;
+  public message: string;
+  public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  public verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registerData = {};
+    this.message = '';
+  }
+
+  ngOnInit(): void {}
+
+  registerUser() {}
+
+  openSnackBarSuccesfull() {}
+
+  openSnackBarError() {}
+}
